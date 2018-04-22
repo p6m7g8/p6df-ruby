e8fdf7e09f5ec98c802f7f87b3477d110eb74c91
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:25:14 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,14 @@
+p6df::modules::ruby::version() { echo "0.0.1" }
+p6df::modules::ruby::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::ruby::external::brew() {
+
+}
+
+p6df::modules::ruby::init() {
+
+}
+
+p6df::modules::ruby::init
